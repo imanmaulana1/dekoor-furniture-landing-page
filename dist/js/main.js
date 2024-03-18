5c1cb2f98dc7e8a7e44954991736a5cd1d198595
Iman Maulana <maulanaiman902@gmail.com>
2024-03-18 17:25:11 +0700

    feat: show modal input register

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,24 +1,41 @@
 const navbar = document.querySelector('.navbar');
 
 const btnHamburger = document.getElementById('btn-hamburger');
-btnHamburger.addEventListener('click', showMenuMobile());
+btnHamburger.addEventListener('click', showMenuMobile);
 
-const btnClose = document.querySelector('.nav__close');
-btnClose.addEventListener('click', closeMenuMobile());
+const navClose = document.querySelector('.nav__close');
+navClose.addEventListener('click', closeMenuMobile);
 
 const navItems = document.querySelectorAll('.nav__item');
 navItems.forEach((item) => {
-  item.addEventListener('click', closeMenuMobile());
+  item.addEventListener('click', closeMenuMobile);
 });
 
+const btnSignup = document.querySelectorAll('.btn__sign');
+btnSignup.forEach((item) => {
+  item.addEventListener('click', () => {
+    closeMenuMobile();
+    showModalForm();
+  });
+});
+
+const overlayForm = document.getElementById('form-register');
+
+const formClose = document.querySelector('.btn__close');
+formClose.addEventListener('click', closeModalForm);
+
+function showModalForm() {
+  overlayForm.classList.add('show');
+}
+
+function closeModalForm() {
+  overlayForm.classList.remove('show');
+}
+
 function showMenuMobile() {
-  return () => {
-    navbar.classList.add('show');
-  };
+  navbar.classList.add('show');
 }
 
 function closeMenuMobile() {
-  return () => {
-    navbar.classList.remove('show');
-  };
+  navbar.classList.remove('show');
 }
