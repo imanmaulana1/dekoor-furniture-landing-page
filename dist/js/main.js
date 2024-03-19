a0a75abaf68a2e5ed259f148c5477d662cca511c
Iman Maulana <maulanaiman902@gmail.com>
2024-03-19 07:01:49 +0700

    style: change tag html form cta

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,55 +1,67 @@
-const navbar = document.querySelector('.navbar');
+function getClass(element, className) {
+  return element.classList.add(className);
+}
+
+function removeClass(element, className) {
+  return element.classList.remove(className);
+}
+
+function showHamburgerMenu() {
+  const navbar = document.querySelector('.navbar');
+  getClass(navbar, 'show');
+}
+
+function closeHamburgerMenu() {
+  const navbar = document.querySelector('.navbar');
+  removeClass(navbar, 'show');
+}
+
+function flipModalInput() {
+  const formWrapper = document.querySelector('.form-wrapper');
+  getClass(formWrapper, 'rotate');
+}
+
+function unFlipModalInput() {
+  const formWrapper = document.querySelector('.form-wrapper');
+  removeClass(formWrapper, 'rotate');
+}
+
+function showModalInput() {
+  const overlayForm = document.getElementById('form-register');
+  getClass(overlayForm, 'show');
+}
+
+function closeModalInput() {
+  const overlayForm = document.getElementById('form-register');
+  removeClass(overlayForm, 'show');
+}
 
 const btnHamburger = document.getElementById('btn-hamburger');
-btnHamburger.addEventListener('click', showMenuMobile);
+btnHamburger.addEventListener('click', showHamburgerMenu);
 
 const navClose = document.querySelector('.nav__close');
-navClose.addEventListener('click', closeMenuMobile);
+navClose.addEventListener('click', closeHamburgerMenu);
 
 const navItems = document.querySelectorAll('.nav__item');
 navItems.forEach((item) => {
-  item.addEventListener('click', closeMenuMobile);
+  item.addEventListener('click', closeHamburgerMenu);
 });
 
 const btnSignup = document.querySelectorAll('.btn__sign');
 btnSignup.forEach((item) => {
   item.addEventListener('click', () => {
-    closeMenuMobile();
-    showModalForm();
+    closeHamburgerMenu();
+    showModalInput();
   });
 });
 
-const overlayForm = document.getElementById('form-register');
-
 const btnCloseForm = document.querySelectorAll('.btn__close');
 btnCloseForm.forEach((item) => {
-  item.addEventListener('click', closeModalForm);
+  item.addEventListener('click', closeModalInput);
 });
 
 const login = document.getElementById('login');
-login.addEventListener('click', () => {
-  const formWrapper = document.querySelector('.form-wrapper');
-  formWrapper.classList.add('rotate');
-});
+login.addEventListener('click', flipModalInput);
 
 const register = document.getElementById('register');
-register.addEventListener('click', () => {
-  const formWrapper = document.querySelector('.form-wrapper');
-  formWrapper.classList.remove('rotate');
-});
-
-function showModalForm() {
-  overlayForm.classList.add('show');
-}
-
-function closeModalForm() {
-  overlayForm.classList.remove('show');
-}
-
-function showMenuMobile() {
-  navbar.classList.add('show');
-}
-
-function closeMenuMobile() {
-  navbar.classList.remove('show');
-}
+register.addEventListener('click', unFlipModalInput);
